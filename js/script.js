61ebb6fb0db7c51890a546a1208880360a4f5a54
abdicore <abdirhamanmohamed@gmail.com>
2024-08-26 10:51:49 +0200

    restyle-layout

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 # MILESTONE 1 ok
 Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-#MILESTONE 2
+#MILESTONE 2 ok
 Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
 #MILESTONE 3
 In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
@@ -16,12 +16,16 @@ Al click sulla cella, stampiamo il numero della cella cliccata in console, poi c
 
 
 /*
-#MILESTONE 2
+#MILESTONE 2 ok
 Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, 
 vengono generate 100 celle in 10 righe da 10 celle ciascuna.
 */
 
 //Dichiarazioni funzioni
+
+function createCell (){
+
+}
 
 
 //Fase di preparazione
@@ -29,7 +33,8 @@ vengono generate 100 celle in 10 righe da 10 celle ciascuna.
 
 //richiamo gli elemnti dal dom
 const grid = document.getElementById('grid')
-const buttun = document.querySelector('button')
+const button = document.querySelector('button')
+const select = document.getElementById('select')
 
 //Elementi inizialai
 
@@ -37,18 +42,24 @@ const rows = 10;
 const cols = 10;
 const totalCell = rows * cols;
 
-//Regiamo al click
-buttun.addEventListener('click', function (){
-    // creco un ciclo per il numero di totalCell
-    for ( let i = 1; i <=totalCell; i++ ){
-        //creo le cell
+// Reagiamo al click
+button.addEventListener('click', function() {
+    // Rimuovo la classe d-none dalla select
+    select.classList.remove('d-none');
+
+    // Pulisco la griglia prima di generare nuove celle
+    grid.innerHTML = '';
+
+    // Creo un ciclo per il numero di totalCell
+    for (let i = 1; i <= totalCell; i++) {
+        // Creo la cella
         const cell = document.createElement('div');
-        //aggiungo la classe
+        // Aggiungo la classe alla cella
         cell.className = 'cell';
-        //inserisco in oagina
-        grid.appendChild(cell)
+        // Inserisco il numero progressivo nella cella
+        cell.textContent = i;
 
+        // Inserisco la cella nella griglia
+        grid.appendChild(cell);
     }
-
-})
-
+});
